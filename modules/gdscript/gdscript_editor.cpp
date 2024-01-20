0fd80f96e5b6f7bbaf552a086c3356a5d0e56975
kaluluosi111 <kaluluosi@gmail.com>
2024-01-20 18:18:07 +0800

    remove quote of InputAction

--- modules/gdscript/gdscript_editor.cpp
+++ modules/gdscript/gdscript_editor.cpp
@@ -2637,8 +2637,8 @@ static void _find_call_arguments(GDScriptParser::CompletionContext &p_context, c
 							continue;
 						}
 						String name = s.get_slice("/", 1);
-						ScriptLanguage::CodeCompletionOption option(name, ScriptLanguage::CODE_COMPLETION_KIND_FUNCTION);
-						option.insert_text = option.display.quote(quote_style);
+						ScriptLanguage::CodeCompletionOption option(name, ScriptLanguage::CODE_COMPLETION_KIND_CONSTANT);
+						option.insert_text = option.display;
 						r_result.insert(option.display, option);
 					}
 				}
